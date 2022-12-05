a58da559470df09ef1203ce894e7b6d77e4340b4
JP Veilleux <jpveix@gmail.com>
2022-12-04 22:13:11 -0500

    Day 04 - Part 2

--- src/day04/Day04.kt
+++ src/day04/Day04.kt
@@ -49,12 +49,18 @@ fun main() {
             val pair1 = pairArray[0].split("-")
             val pair2 = pairArray[1].split("-")
 
-            if (pair1[0].toInt() >= pair2[0].toInt() || pair1[1].toInt() <= pair2[1].toInt()) {
+            val pair1Part1 = pair1[0].toInt();
+            val pair1Part2 = pair1[1].toInt();
+
+            val pair2Part1 = pair2[0].toInt();
+            val pair2Part2 = pair2[1].toInt();
+
+            if (pair1Part1 in pair2Part1..pair2Part2 || pair1Part2 in pair2Part1..pair2Part2) {
                 nbrFound++
                 println("------------------------------")
                 println("FP Overlaps SP - $pairArray")
                 println("------------------------------")
-            } else if ((pair1[1].toInt() >= pair2[1].toInt()) || (pair1[0].toInt() <= pair2[0].toInt())) {
+            } else if (pair2Part1 in pair1Part1..pair1Part2 || pair2Part2 in pair1Part1..pair1Part2) {
                 nbrFound++
                 println("------------------------------")
                 println("SP Overlaps FP - $pairArray")
@@ -69,12 +75,12 @@ fun main() {
 
 
 
-    check(part1(part1controlInput) == 2)
+    //check(part1(part1controlInput) == 2)
     check(part2(part1controlInput) == 4)
 
 
-    //part1(part1JPcontrolInput)
+    //part2(part1controlInput);
 
-    part1(input)
+    //part1(input)
     part2(input)
 }
