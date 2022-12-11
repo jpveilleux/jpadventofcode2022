bdec11833c557b71419d01ef3472180005fcb399
JP Veilleux <jpveix@gmail.com>
2022-12-11 17:48:02 -0500

    Day 10 - Changed name of control file

--- src/utils/Setup.kt
+++ src/utils/Setup.kt
@@ -4,7 +4,7 @@ import readInput
 
 class Setup (dayNumber: Int) {
     private var baseDir: String = "./day${dayNumber}/"
-    private var controlInputFileName: String = "${baseDir}Day${dayNumber}_part1_control"
+    private var controlInputFileName: String = "${baseDir}Day${dayNumber}_control"
     private var inputFileName: String = "${baseDir}Day0${dayNumber}"
     val controlInput = readInput(controlInputFileName)
     val input = readInput(inputFileName)
