d9431035f5fb1f6755a768a904ce891e4210442e
JP Veilleux <jpveix@gmail.com>
2022-12-11 17:16:26 -0500

    Day 10 - Part 1 working, part 2 well under way

--- src/day10/Day10.kt
+++ src/day10/Day10.kt
@@ -27,21 +27,65 @@ val input = readInput(inputFileName)
 
 
 data class SignalStrength (var signalStrength: Int)
-
+class Sprite (var position: List<Int>) {
+    fun getCenter (): Int {
+        if (position.size > 3) {
+            return position[0]
+        } else {
+            return position[1]
+        }
+    }
+}
 
 fun main () {
-    
+    fun cycleModTo20(multiple: Int, cycles: Int): Boolean {
+        return cycles % multiple == 20
+    }
     fun updateX(cycles: Int, x: Int, sigStrObject: SignalStrength) {
-        println(cycles)
-        
-        if (cycles >= 20 && (cycles % 40 == 20)) {
-            println("x: $x")
-            println("stating 20 then every 40: $cycles")
+        if (cycles >= 20 && cycleModTo20(40, cycles)) {
             sigStrObject.signalStrength += (x * cycles)
         }
     }
     
-    fun part1 (input: List<String>) {
+    fun cycleIsDivisibleBy(multiple: Int, cycles: Int): Boolean {
+        return cycles % multiple == 0
+    }
+    
+    fun updateSpritePos(x: Int, sprite: Sprite) {
+        sprite.position = listOf(x - 1, x, x + 1)
+    }
+    
+    fun getPixelToDraw(currentScreenLine: MutableList<String>, sprite: Sprite): String {
+        var currentPixelPos = currentScreenLine.size
+        
+        if (currentPixelPos in sprite.position) {
+            return "#"
+        }
+        
+        return "."
+    }
+    
+    fun updateScreen(
+        cycles: Int,
+        x: Int,
+        screenLines: MutableList<MutableList<String>>,
+        currentScreenLine: MutableList<String>,
+        sprite: Sprite
+    ) {
+        if (cycles > 20 && cycleIsDivisibleBy(40, cycles)) {
+            println("updateScreen cycles: $cycles")
+            
+            currentScreenLine.add(getPixelToDraw(currentScreenLine, sprite))
+            val finishedScreenLine = currentScreenLine.toMutableList()
+            
+            screenLines.add(finishedScreenLine)
+            currentScreenLine.clear()
+        } else {
+            currentScreenLine.add(getPixelToDraw(currentScreenLine, sprite))
+        }
+    }
+    
+    fun part1 (input: List<String>): Int {
         var x = 1
         var cmdNbr = 0
         var cycles = 0
@@ -69,16 +113,58 @@ fun main () {
                 updateX(cycles, x, signalStr)
             }
         }
-    
-        println("cycles: $cycles")
-        println("X: $x")
-        println("Final signal strength: ${signalStr.signalStrength}")
+        
+        return signalStr.signalStrength
     }
     
     fun part2 (input: List<String>) {
+        var x = 1
+        var cmdNbr = 0
+        var cycles = 0
+        val signalStr = SignalStrength(0)
+        val sprite = Sprite(listOf(0,1,2))
     
+        val screenLines = mutableListOf<MutableList<String>>()
+        val currentScreenLine = mutableListOf<String>()
+    
+        input.map {line ->
+            cmdNbr++
+        
+            val splitCmd = line.split(" ")
+        
+            if (splitCmd.size > 1) {
+                val cmd = splitCmd[0]
+                val changeAmount = splitCmd[1].toInt()
+            
+                for (cycle in 1..2) {
+                    cycles++
+                    
+                    if (cycle == 2) {
+                        x += changeAmount
+                        updateSpritePos(x, sprite)
+                    }
+                    
+                    updateScreen(cycles, x, screenLines, currentScreenLine, sprite)
+                }
+            } else {
+                cycles++
+                updateScreen(cycles, x, screenLines, currentScreenLine, sprite)
+            }
+        }
+    
+        println("Total cycles: $cycles")
+        println("Total x: $x")
+        println("screen lines: $screenLines")
+    
+        screenLines.map {line ->
+            val lineToPrint = line.joinToString("")
+            println(lineToPrint)
+        }
     }
     
-    //part1(part1controlInput)
-    part1(input)
+    check(part1(part1controlInput) == 13140)
+    check(part1(input) == 16020)
+    
+    part2(part1controlInput)
+    //part2(input)
 }
