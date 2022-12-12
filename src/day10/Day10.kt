c6ec9224278428ddd7d5ef81ae0f1f54733afd62
JP Veilleux <jpveix@gmail.com>
2022-12-11 21:29:56 -0500

    Day 10 - Done!

--- src/day10/Day10.kt
+++ src/day10/Day10.kt
@@ -1,6 +1,5 @@
 package day10
 
-import readInput
 import utils.Setup
 
 val daySetup = Setup(10)
@@ -30,12 +29,15 @@ fun main () {
         return cycles % multiple == 0
     }
     
-    fun updateSpritePos(x: Int, sprite: Sprite) {
+    fun updateSpritePosition(x: Int, sprite: Sprite) {
         sprite.position = listOf(x - 1, x, x + 1)
+        println("update sprite position - sprite pos: ${sprite.position}")
     }
     
     fun getPixelToDraw(currentScreenLine: MutableList<String>, sprite: Sprite): String {
         var currentPixelPos = currentScreenLine.size
+        
+        println("PixelToDraw - currentPixelPos: ${currentPixelPos + 1} - sprite pos: ${sprite.position}")
         
         if (currentPixelPos in sprite.position) {
             return "#"
@@ -52,15 +54,15 @@ fun main () {
         sprite: Sprite
     ) {
         if (cycles > 20 && cycleIsDivisibleBy(40, cycles)) {
-            println("updateScreen cycles: $cycles")
-            
             currentScreenLine.add(getPixelToDraw(currentScreenLine, sprite))
             val finishedScreenLine = currentScreenLine.toMutableList()
             
             screenLines.add(finishedScreenLine)
             currentScreenLine.clear()
+            println("EOL - cycle: $cycles")
         } else {
             currentScreenLine.add(getPixelToDraw(currentScreenLine, sprite))
+            println("cycle: $cycles")
         }
     }
     
@@ -100,7 +102,6 @@ fun main () {
         var x = 1
         var cmdNbr = 0
         var cycles = 0
-        val signalStr = SignalStrength(0)
         val sprite = Sprite(listOf(0,1,2))
     
         val screenLines = mutableListOf<MutableList<String>>()
@@ -112,18 +113,17 @@ fun main () {
             val splitCmd = line.split(" ")
         
             if (splitCmd.size > 1) {
-                val cmd = splitCmd[0]
                 val changeAmount = splitCmd[1].toInt()
             
                 for (cycle in 1..2) {
                     cycles++
                     
+                    updateScreen(cycles, x, screenLines, currentScreenLine, sprite)
+                    
                     if (cycle == 2) {
                         x += changeAmount
-                        updateSpritePos(x, sprite)
+                        updateSpritePosition(x, sprite)
                     }
-                    
-                    updateScreen(cycles, x, screenLines, currentScreenLine, sprite)
                 }
             } else {
                 cycles++
@@ -144,6 +144,6 @@ fun main () {
     check(part1(daySetup.controlInput) == 13140)
     check(part1(daySetup.input) == 16020)
     
-    part2(daySetup.controlInput)
-    //part2(input)
+    //part2(daySetup.controlInput)
+    part2(daySetup.input)
 }
--- src/utils/Setup.kt
+++ src/utils/Setup.kt
@@ -5,7 +5,7 @@ import readInput
 class Setup (dayNumber: Int) {
     private var baseDir: String = "./day${dayNumber}/"
     private var controlInputFileName: String = "${baseDir}Day${dayNumber}_control"
-    private var inputFileName: String = "${baseDir}Day0${dayNumber}"
+    private var inputFileName: String = "${baseDir}Day${dayNumber}"
     val controlInput = readInput(controlInputFileName)
     val input = readInput(inputFileName)
 }
